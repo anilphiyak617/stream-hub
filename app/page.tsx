cf3a7177bbdaab1eccf819ab59c1c0489e61ecff
anilphiyak617 <anilphiyak617@gmail.com>
2024-01-16 11:56:26 +0530

    CONFIGURED: shadCn ui library settup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
+
+interface typeA {
+  name: number;
+  book:string;
+}
+
+interface typeB {
+  roll:number
+}
+
+interface typeC extends typeA,typeB{
+  address: string;
+}
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Check 
+      <Button variant={'destructive'}> this is a sample button</Button>
     </main>
   )
 }
